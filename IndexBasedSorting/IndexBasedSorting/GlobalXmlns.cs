3432973561a64602774fd3321412260340b66f9f
KerubananthanGanesan <kerubananthan.ganesan@syncfusion.com>
2025-10-17 15:51:57 +0530

    987993[Maui Kanban] Added index based sorting sample

---
+++ IndexBasedSorting/IndexBasedSorting/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "IndexBasedSorting")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "IndexBasedSorting.Pages")]
---
+++ IndexBasedSorting/IndexBasedSorting/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IndexBasedSorting
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
